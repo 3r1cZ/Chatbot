c2ffa1e5a8f89702da6e22067f4e2d1901089226
3r1cZ <114354109+3r1cZ@users.noreply.github.com>
2023-09-10 14:14:45 -0400

    Implement self-attention Jupyter code into bigram model with Python script

--- gpt.py
+++ gpt.py
@@ -5,9 +5,9 @@ from torch.nn import functional as F
 # hyperparameters
 batch_size = 32 # number of independent sequences being processed in parallel
 block_size = 8 # the maximum context length for predictions
-max_iters = 3000
-eval_interval = 300
-learning_rate = 1e-2
+max_iters = 5000
+eval_interval = 500
+learning_rate = 1e-3
 device = 'cuda' if torch.cuda.is_available() else 'cpu' # runs on gpu, else cpu
 eval_iters = 200
 n_embd = 32
@@ -58,6 +58,31 @@ def estimate_loss():
     model.train()
     return out
 
+class Head(nn.Module):
+    """ one head of self-attention """
+
+    def __init__(self, head_size):
+        super().__init__()
+        self.key = nn.Linear(n_embd, head_size, bias=False)
+        self.query = nn.Linear(n_embd, head_size, bias=False)
+        self.value = nn.Linear(n_embd, head_size, bias=False)
+        self.register_buffer('tril', torch.tril(torch.ones(block_size, block_size))) # lower triangular matrix
+
+    # input of size (batch, time-step, channels)
+    # output of size (batch, time-step, head size)
+    def forward(self, x):
+        B,T,C = x.shape
+        k = self.key(x)   # (B,T,hs)
+        q = self.query(x) # (B,T,hs)
+        # compute attention scores ("affinities") | weight calculation providing averages for past and current tokens in a data-dependent way
+        wei = q @ k.transpose(-2,-1) * k.shape[-1]**-0.5 # matrix multiplication: (B, T, hs) @ (B, hs, T) -> (B, T, T)
+        wei = wei.masked_fill(self.tril[:T, :T] == 0, float('-inf')) # (B, T, T) , triangular masking
+        wei = F.softmax(wei, dim=-1) # (B, T, T) , exponentiates and normalizes in order to create the weighting (-inf will turn into 0)
+        # perform the weighted aggregation of the values
+        v = self.value(x) # (B,T,hs) , aggregated elements
+        out = wei @ v # (B, T, T) @ (B, T, hs) -> (B, T, hs) , changes output to head_size dimensions
+        return out
+
 # simple bigram model
 class BigramLanguageModel(nn.Module):
 
@@ -66,6 +91,7 @@ class BigramLanguageModel(nn.Module):
         # each token directly reads off the logits for the next token from a lookup table
         self.token_embedding_table = nn.Embedding(vocab_size, n_embd)
         self.position_embedding_table = nn.Embedding(block_size, n_embd)
+        self.sa_head = Head(n_embd) # self-attention head
         self.lm_head = nn.Linear(n_embd, vocab_size) # language modelling head
 
     def forward(self, idx, targets=None):
@@ -75,6 +101,7 @@ class BigramLanguageModel(nn.Module):
         tok_emb = self.token_embedding_table(idx) # (B,T,C)
         pos_emb = self.position_embedding_table(torch.arange(T, device=device)) # (T,C)
         x = tok_emb + pos_emb # (B,T,C) , x holds token identities and positions at which they occur
+        x = self.sa_head(x) # apply one head of self-attention (B,T,C)
         logits = self.lm_head(x) # (B,T,vocab_size)
 
         if targets is None:
@@ -92,8 +119,10 @@ class BigramLanguageModel(nn.Module):
     def generate(self, idx, max_new_tokens):
         # idx is (B, T) array of indices in the current context
         for _ in range(max_new_tokens):
+            # adjust idx to fix last block_size tokens
+            idx_cond = idx[:, -block_size:]
             # get the predictions
-            logits, loss = self(idx)
+            logits, loss = self(idx_cond)
             # focus only on the last time step
             logits = logits[:, -1, :] # becomes (B, C)
             # apply softmax to get probabilities
