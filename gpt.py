222540d327758d5d99cc18454a058b45bafdf000
3r1cZ <114354109+3r1cZ@users.noreply.github.com>
2023-09-10 14:56:25 -0400

    Implement feed-forward network

--- gpt.py
+++ gpt.py
@@ -58,8 +58,8 @@ def estimate_loss():
     model.train()
     return out
 
+# one head of self-attention
 class Head(nn.Module):
-    """ one head of self-attention """
 
     def __init__(self, head_size):
         super().__init__()
@@ -83,8 +83,8 @@ class Head(nn.Module):
         out = wei @ v # (B, T, T) @ (B, T, hs) -> (B, T, hs) , changes output to head_size dimensions
         return out
 
+# multiple heads of self-attention in parallel
 class MultiHeadAttention(nn.Module):
-    """ multiple heads of self-attention in parallel """
 
     def __init__(self, num_heads, head_size):
         super().__init__()
@@ -93,6 +93,19 @@ class MultiHeadAttention(nn.Module):
     def forward(self, x):
         out = torch.cat([h(x) for h in self.heads], dim=-1) # concatenate outputs from each head
         return out
+
+# a simple linear layer followed by a non-linearity
+class FeedForward(nn.Module):
+
+    def __init__(self, n_embd):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(n_embd, n_embd),
+            nn.ReLU(), # rectifier activation
+        )
+
+    def forward(self, x):
+        return self.net(x)
 
 # simple bigram model
 class BigramLanguageModel(nn.Module):
@@ -103,6 +116,7 @@ class BigramLanguageModel(nn.Module):
         self.token_embedding_table = nn.Embedding(vocab_size, n_embd)
         self.position_embedding_table = nn.Embedding(block_size, n_embd)
         self.sa_heads = MultiHeadAttention(4, n_embd//4) # 4 heads of 8-dimensional self-attention
+        self.ffwd = FeedForward(n_embd)
         self.lm_head = nn.Linear(n_embd, vocab_size) # language modelling head
 
     def forward(self, idx, targets=None):
@@ -113,6 +127,7 @@ class BigramLanguageModel(nn.Module):
         pos_emb = self.position_embedding_table(torch.arange(T, device=device)) # (T,C)
         x = tok_emb + pos_emb # (B,T,C) , x holds token identities and positions at which they occur
         x = self.sa_heads(x) # apply one head of self-attention (B,T,C)
+        x = self.ffwd(x) # (B,T,C)
         logits = self.lm_head(x) # (B,T,vocab_size)
 
         if targets is None:
